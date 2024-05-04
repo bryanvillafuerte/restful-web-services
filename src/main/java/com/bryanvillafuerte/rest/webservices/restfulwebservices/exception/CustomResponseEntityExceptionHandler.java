a0e6ae6aed356eb386c56a1cd6959d58ef1f4488
Bryan Luis Villafuerte <bryan.villafuerte@inmeta.no>
2024-05-04 11:58:11 +0200

    implemented validations for Rest API

--- src/main/java/com/bryanvillafuerte/rest/webservices/restfulwebservices/exception/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/bryanvillafuerte/rest/webservices/restfulwebservices/exception/CustomResponseEntityExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.bryanvillafuerte.rest.webservices.restfulwebservices.exception;
 
 import com.bryanvillafuerte.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @ControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -25,6 +29,13 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), "Totol Errors: " + ex.getErrorCount() + "." + " First error: " + Objects.requireNonNull(ex.getFieldError()).getDefaultMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
 }
